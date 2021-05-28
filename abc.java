43d9f6d8ff2411e28c5c7eba4e8affb5c8613fb1
Pritesh Chavan <pritesh.chavan@ibm.com>
2021-05-28 13:16:43 +0530

    MODIFIED abc and xyz and altered ztoz

--- abc.java
+++ abc.java
@@ -2,6 +2,7 @@ public class abc
 {
 	public static void main(String args[])
 	{
+		System.out.println("Hieee");
 		for (int i =1;i<=10;i++)
 		{
 		System.out.println("Hello");
--- xyz.java
+++ xyz.java
@@ -1,5 +1,6 @@
 public class xyz
 {
+	Sysytem.out.println("neww");
 	public void abc()
 	{
 		Sysytem.out.println("FRom XYZ");
